badf33856e89b90598ebd7022c81cd5f35637421
Laf111 <47532310+Laf111@users.noreply.github.com>
2019-02-18 21:30:49 +0100

    fix Invoke-WebRequest (not using the Internet Explorer engine for parsing websites)

--- resources/ps1/updateGP.ps1
+++ resources/ps1/updateGP.ps1
@@ -1,7 +1,7 @@
 # This script have to be copied in _BatchFW_Graphic_Packs
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$latestRelease = Invoke-WebRequest https://github.com/slashiee/cemu_graphic_packs/releases/latest -Headers @{"Accept"="application/json"}
+$latestRelease = Invoke-WebRequest -UseBasicParsing https://github.com/slashiee/cemu_graphic_packs/releases/latest -Headers @{"Accept"="application/json"}
 # The releases are returned in the format {"id":3622206,"tag_name":"hello-1.0.0.11",...}, we have to extract the tag_name.
 $json = $latestRelease.Content | ConvertFrom-Json
 $latestVersion = $json.tag_name
