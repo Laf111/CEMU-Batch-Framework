1e43d1be7a6be8467412fa3d1b84c15e096f1b24
Laf111 <47532310+Laf111@users.noreply.github.com>
2019-09-27 23:03:05 +0200

    V15

--- tools/forcePriority.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal EnableExtensions
-timeout /t 3 > NUL 2>&1
-REM : force riority to High
-wmic process where name="cemu.exe" call setpriority 128 > NUL 2>&1
-REM : force riority to Realtime (will work only when launching as admin)
-wmic process where name="cemu.exe" call setpriority 256 > NUL 2>&1
-exit 0
