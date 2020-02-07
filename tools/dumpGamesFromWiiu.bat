0839c120b337bd3529b1db75e4ba5c0eafb29de0
Laf111 <47532310+Laf111@users.noreply.github.com>
2020-02-08 00:01:57 +0100

    V16-1

--- tools/dumpGamesFromWiiu.bat
+++ tools/dumpGamesFromWiiu.bat
@@ -300,7 +300,7 @@ REM : main
         REM : get the last location from logFile
         set "OUTPUT_FOLDER="NONE""
         for /F "tokens=2 delims=~=" %%i in ('type !logFile! ^| find "Create" 2^>NUL') do set "OUTPUT_FOLDER="%%i""
-        if not ["!OUTPUT_FOLDER!"] == ["NONE"] (
+        if not [!OUTPUT_FOLDER!] == ["NONE"] (
             wscript /nologo !Start! !setup! !OUTPUT_FOLDER!
         ) else (
             wscript /nologo !Start! !setup!
