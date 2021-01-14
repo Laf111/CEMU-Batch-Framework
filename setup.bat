54d14861e7e8f329e7260a753d7b5149080be427
Laf111 <47532310+Laf111@users.noreply.github.com>
2021-01-14 23:34:04 +0100

    V21-4
    14/01/2021 V21-4 :
    add : minimize time of all files copy (add robocopy multithreaded option). Pre and post launching CEMU times are lowered by 30~50% 
          on my 8 cores CPU.
    add : update gamesLibrary.log when scanning games (to remove uninstalled games and avoid conflicts if you re-install them)
    fix : when registering a CEMU install in setup.bat, ask to import mlc01 data only if settings.xml exists 
    fix : clean all fnr logs before exiting launchGame.bat
    fix : remove old script deleteAllInGameSaves.bat left by mistake in the project

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ REM : main
     color 4F
 
     REM : CEMU's Batch FrameWork Version
-    set "BFW_VERSION=V21-3"
+    set "BFW_VERSION=V21-4"
 
     REM : version of GFX packs created
     set "BFW_GFXP_VERSION=6"
