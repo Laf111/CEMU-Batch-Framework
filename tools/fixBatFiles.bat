e77c0ea1d57c40fb74bf56882784864a8d165aa9
Laf111 <47532310+Laf111@users.noreply.github.com>
2021-04-07 14:57:10 +0200

    V22-6
    07/04/2021 V22-6 :
    add : a check on environnement to avoid system with Linux tools in DOS environnement
    fix : modify the check on VBS script launch permission
    fix : regression on graphic pack completion (deactivated by mistake)

--- tools/fixBatFiles.bat
+++ tools/fixBatFiles.bat
@@ -20,7 +20,7 @@ REM : main
     
     REM : ------------------------------------------------------------------
     REM : CEMU's Batch FrameWork Version to produce
-    set "BFW_NEXT_VERSION=V22-5"
+    set "BFW_NEXT_VERSION=V22-6"
 
     set "THIS_SCRIPT=%~0"
 
