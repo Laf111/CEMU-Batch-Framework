5dc8aa80cd98894ee22c4a92bb5646363609a39a
Laf111 <47532310+Laf111@users.noreply.github.com>
2021-07-27 17:41:20 +0200

    V22-7
    28/07/2021 V22-7 :
    add : replace FTPiiU by WiiuFtpServer  V4-3 in WiiuSDcard.rar and code
    add : support new transferable shaders cache (CEMU v1.25) and vulkanPermanentCacheFile
    fix : BacthFw's version not shown in log file
    fix : remove unwanted traces in std_out of wizardFirstSaving.bat and createCapGraphicPacks.bat

--- tools/fixBatFiles.bat
+++ tools/fixBatFiles.bat
@@ -20,7 +20,7 @@ REM : main
     
     REM : ------------------------------------------------------------------
     REM : CEMU's Batch FrameWork Version to produce
-    set "BFW_NEXT_VERSION=V22-6"
+    set "BFW_NEXT_VERSION=V22-7"
 
     set "THIS_SCRIPT=%~0"
 
